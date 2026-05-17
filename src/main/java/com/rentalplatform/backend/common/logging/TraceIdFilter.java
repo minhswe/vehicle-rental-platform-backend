0ffa5948bd38d7c8e63a77232b0204ac373fcdfd
Quang Minh <quangminhswe@gmail.com>
2026-05-17 22:34:54 +0700

    fix(traceId): missing trace id

--- src/main/java/com/rentalplatform/backend/common/logging/TraceIdFilter.java
+++ src/main/java/com/rentalplatform/backend/common/logging/TraceIdFilter.java
@@ -1,26 +1,36 @@
 package com.rentalplatform.backend.common.logging;
 
 import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.slf4j.MDC;
+import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 import java.util.UUID;
 
-public class TraceIdFilter implements Filter {
+public class TraceIdFilter extends OncePerRequestFilter {
     public static final String TRACE_ID = "traceId";
+    public static final String TRACE_ID_HEADER = "X-Trace-Id";
 
     @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
-            throws IOException, ServletException {
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
+                                    FilterChain filterChain) throws ServletException, IOException {
+        String traceId = request.getHeader(TRACE_ID_HEADER);
+
+        if (traceId == null || traceId.isBlank()) {
+            traceId = UUID.randomUUID().toString();
+        }
 
         try {
-            String traceId = UUID.randomUUID().toString();
             MDC.put(TRACE_ID, traceId);
+            response.setHeader(TRACE_ID_HEADER, traceId);
 
-            chain.doFilter(request, response);
+            filterChain.doFilter(request, response);
 
         } finally {
-            MDC.clear();
+            MDC.remove(TRACE_ID);
         }
+
     }
 }
--- src/main/java/com/rentalplatform/backend/config/filter/FilterConfig.java
+++ src/main/java/com/rentalplatform/backend/config/filter/FilterConfig.java
@@ -8,11 +8,17 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class FilterConfig {
     @Bean
+    public TraceIdFilter traceIdFilterBean() {
+        return new TraceIdFilter();
+    }
+
+    @Bean
     public FilterRegistrationBean<TraceIdFilter> traceIdFilter() {
         FilterRegistrationBean<TraceIdFilter> registration = new FilterRegistrationBean<>();
 
         registration.setFilter(new TraceIdFilter());
         registration.addUrlPatterns("/*"); // apply to all api
+        registration.setName("traceIdFilter");
         registration.setOrder(1); // high priority
 
         return registration;
