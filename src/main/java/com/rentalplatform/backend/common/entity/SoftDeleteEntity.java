5cc9f397d991b742b2b4d068bfaedc30bbcf653c
Quang Minh <quangminhswe@gmail.com>
2026-06-10 21:48:38 +0700

    refactor(entity): split BaseEntity into AuditEntity and SoftDeleteEntity

--- src/main/java/com/rentalplatform/backend/common/entity/SoftDeleteEntity.java
+++ src/main/java/com/rentalplatform/backend/common/entity/SoftDeleteEntity.java
@@ -5,26 +5,15 @@ import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.Instant;
 
 @MappedSuperclass
-@EntityListeners(AuditingEntityListener.class)
 @Getter
 @Setter
-public abstract class BaseEntity {
-
-    @CreatedDate
-    @Column(updatable = false)
-    private Instant createdAt;
-
-    @LastModifiedDate
-    @Column(nullable = false)
-    private Instant updatedAt;
-
+@EntityListeners(AuditingEntityListener.class)
+public abstract class SoftDeleteEntity extends AuditEntity {
 
     @Column(nullable = false)
     private boolean deleted = false;
